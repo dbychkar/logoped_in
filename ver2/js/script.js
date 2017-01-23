4c84597fef5b6ad6b6c249a097c235c74447da30
dbychkar <dbychkar@gmail.com>
2017-01-23 11:25:54 +0300

    lint scrtipt.js

--- ver2/js/script.js
+++ ver2/js/script.js
@@ -1,27 +1,25 @@
 /* Activates when scrolled */
-document.addEventListener("scroll", yScroll);
+document.addEventListener('scroll', yScroll);
 
 function yScroll() {
-
-  var yPos = window.pageYOffset;
+  const yPos = window.pageYOffset;
 
   if (yPos > 10) {
-    document.getElementById("top-btn").style.bottom = "10px";
+    document.getElementById('top-btn').style.bottom = '10px';
   } else {
-    document.getElementById("top-btn").style.bottom = "-50px";
+    document.getElementById('top-btn').style.bottom = '-50px';
   }
 }
 
 function ScrollTop() {
+  let i = window.pageYOffset;
 
-  var i = window.pageYOffset;
-
-  var intervalTimer = setInterval(function() {
+  var intervalTimer = setInterval(() => {
     if (i > 0) {
       window.scrollTo(0, i);
       i -= 15;
     } else {
-      document.getElementById("top-btn").style.bottom = "-50px";
+      document.getElementById('top-btn').style.bottom = '-50px';
       clearInterval(intervalTimer);
     }
   }, 0.5);
