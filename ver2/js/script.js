757911d74d713cc8bc0785dd5c616a811cc39d86
dbychkar <dbychkar@gmail.com>
2017-02-13 17:06:25 +0300

    lint scrtipt.js

--- ver2/js/script.js
+++ ver2/js/script.js
@@ -2,25 +2,25 @@
 document.addEventListener('scroll', yScroll);
 
 function yScroll() {
-  const yPos = window.pageYOffset;
+    const yPos = window.pageYOffset;
 
-  if (yPos > 10) {
-    document.getElementById('top-btn').style.bottom = '10px';
-  } else {
-    document.getElementById('top-btn').style.bottom = '-50px';
-  }
+    if (yPos > 10) {
+        document.getElementById('top-btn').style.bottom = '10px';
+    } else {
+        document.getElementById('top-btn').style.bottom = '-50px';
+    }
 }
 
 function ScrollTop() {
-  let i = window.pageYOffset;
+    let i = window.pageYOffset;
 
-  var intervalTimer = setInterval(() => {
-    if (i > 0) {
-      window.scrollTo(0, i);
-      i -= 15;
+    var intervalTimer = setInterval(() => {
+            if (i > 0) {
+        window.scrollTo(0, i);
+        i -= 15;
     } else {
-      document.getElementById('top-btn').style.bottom = '-50px';
-      clearInterval(intervalTimer);
+        document.getElementById('top-btn').style.bottom = '-50px';
+        clearInterval(intervalTimer);
     }
-  }, 0.5);
+}, 0.5);
 }
